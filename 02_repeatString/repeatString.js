3ef4a3f9240a3f4987ea71b869e527eb1580441e
zuengi <165200204+zuengi@users.noreply.github.com>
2025-07-20 23:45:35 +0800

    Updated the repository

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(input, numb) {
+if (numb < 0) { return 'error'}
+let string = "";
+for (let i = 0; i < numb; i++)
+{
+    string += input;
+}
+return string;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    return input.split('').reverse().join('');
 };
 
 // Do not edit below this line
